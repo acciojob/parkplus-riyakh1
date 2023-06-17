a8c2257314f99bd5f6705eacc102e1d5e250c13f
Riya Khan <riyakhan04630@gmail.com>
2023-06-17 20:08:18 +0530

    ParkPlus Assignment Done

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.driver.services.impl;
 
+import com.driver.model.User;
 import com.driver.repository.UserRepository;
 import com.driver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,39 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository4;
+
     @Override
     public void deleteUser(Integer userId) {
+
+        userRepository4.deleteById(userId);
+
 
     }
 
     @Override
     public User updatePassword(Integer userId, String password) {
+        User updatedUser;
+
+        updatedUser = userRepository4.findById(userId).get();
+
+        updatedUser.setPassword(password);
+
+        userRepository4.save(updatedUser);
+
+        return updatedUser;
 
     }
 
     @Override
     public void register(String name, String phoneNumber, String password) {
+
+        User user = new User();
+        user.setName(name);
+        user.setPhoneNumber(phoneNumber);
+        user.setPassword(password);
+
+        userRepository4.save(user);
+
 
     }
 }
